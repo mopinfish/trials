c48056885a9e212f8a24f29193574259b2d9d491
otsukano <otsukanoboru@next-group.jp>
2015-09-01 13:59:45 +0900

    [trial-rspecbook] added implementation and specs and features for guess method.

--- rspecbook/v2/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ rspecbook/v2/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -19,21 +19,22 @@ end
 
 When(/^I start a new game$/) do
   game = Codebreaker::Game.new(pseudo_output)
-  game.start
+  game.start('1234')
 end
 
 Then(/^I should see "([^"]*)"$/) do |message|
   expect(pseudo_output.messages).to include(message)
 end
 
-Given(/^the secret code is "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^the secret code is "([^"]*)"$/) do |secret|
+  @game = Codebreaker::Game.new(pseudo_output)
+  @game.start(secret)
 end
 
-When(/^I guess "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I guess "([^"]*)"$/) do |guess|
+  @game.guess(guess)
 end
 
-Then(/^the mark should be "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^the mark should be "([^"]*)"$/) do |mark|
+  expect(pseudo_output.messages).to include(mark)
 end
--- rspecbook/v2/codebreaker/spec/codebreaker/game_spec.rb
+++ rspecbook/v2/codebreaker/spec/codebreaker/game_spec.rb
@@ -8,12 +8,54 @@ module Codebreaker
     describe '#start' do
       it 'sends a welcome message' do
         expect(output).to receive(:puts).with('Welcome to Codebreaker!')
-        game.start
+        game.start('1234')
       end
 
       it 'prompts for the first guess' do
         expect(output).to receive(:puts).with('Enter guess:')
-        game.start
+        game.start('1234')
+      end
+    end
+
+    describe '#guess' do
+      context 'with no matches' do
+        it "sends a mark with ''" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+
+      context 'with 1 number match' do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+
+      context 'with 1 exact match' do
+        it "sends a mark with '+'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+
+      context 'with 2 number matches' do
+        it "sends a mark with '--'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('--')
+          game.guess('2355')
+        end
+      end
+
+      context 'with 1 number match and 1 exact match (in that order)' do
+        it "sends a mark with '+-'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('+-')
+          game.guess('2535')
+        end
       end
     end
   end
