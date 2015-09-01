ed7ee86bd6daf943002d67011d9535370189f266
otsukano <otsukanoboru@next-group.jp>
2015-09-01 14:31:10 +0900

    [trial-rspecbook] refactored by marker class.

--- rspecbook/v2/codebreaker/lib/codebreaker/game.rb
+++ rspecbook/v2/codebreaker/lib/codebreaker/game.rb
@@ -11,27 +11,9 @@ module Codebreaker
     end
 
     def guess(guess)
-      exact_match_count = 0
-      number_match_count = 0
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          exact_match_count += 1
-        end
-      end
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
-      end
-      @output.puts '+' * exact_match_count + '-' * number_match_count
-    end
-
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
-    end
-
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
+      marker = Marker.new(@secret, guess)
+      @output.puts '+' * marker.exact_match_count +
+                   '-' * marker.number_match_count
     end
   end
 end
