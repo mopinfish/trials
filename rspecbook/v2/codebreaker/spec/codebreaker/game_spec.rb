287d19aacf80ce11d2be059042a503abb40027c7
otsukano <otsukanoboru@next-group.jp>
2015-09-02 10:05:39 +0900

    [trial-rspecbook] refactored spec files.

--- rspecbook/v2/codebreaker/spec/codebreaker/game_spec.rb
+++ rspecbook/v2/codebreaker/spec/codebreaker/game_spec.rb
@@ -3,25 +3,34 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     let(:output) { double('output').as_null_object }
-    let(:game) { Game.new(output) }
+    let(:target) { described_class.new(output) }
+    let(:secret) { '1234' }
 
     describe '#start' do
+      subject { target.start(secret) }
+
       it 'sends a welcome message' do
         expect(output).to receive(:puts).with('Welcome to Codebreaker!')
-        game.start('1234')
+        subject
       end
 
       it 'prompts for the first guess' do
         expect(output).to receive(:puts).with('Enter guess:')
-        game.start('1234')
+        subject
       end
     end
 
     describe '#guess' do
+      let(:guess) { '1234' }
+      subject { target.guess(guess) }
+
+      before :each do
+        target.start(secret)
+      end
+
       it 'sends the mark to output' do
-        game.start('1234')
         expect(output).to receive(:puts).with('++++')
-        game.guess('1234')
+        subject
       end
     end
   end
