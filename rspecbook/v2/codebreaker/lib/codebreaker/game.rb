fce6c643b3269c2ace77973e4c25f491e898948c
otsukano <otsukanoboru@next-group.jp>
2015-09-03 09:50:00 +0900

    [trial-rspecbook] add comments.

--- rspecbook/v2/codebreaker/lib/codebreaker/game.rb
+++ rspecbook/v2/codebreaker/lib/codebreaker/game.rb
@@ -1,15 +1,21 @@
 module Codebreaker
   class Game
+    # 初期化処理
+    # @param [Object] output 出力クラス
     def initialize(output)
       @output = output
     end
 
+    # ゲーム開始処理
+    # @param [String] secret 暗号文字列
     def start(secret)
       @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
 
+    # 回答判定処理
+    # @param [String] guess  推測文字列
     def guess(guess)
       marker = Marker.new(@secret, guess)
       @output.puts '+' * marker.exact_match_count +
